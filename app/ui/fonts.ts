e7d6b3d3641e4ca3df61d7b70b56a6b8ee93ce9d
bofletcher <bfletcher@blackairplane.com>
2024-12-30 10:52:30 -0500

    Added code.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Zen_Kaku_Gothic_Antique } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: "400",
+});
+export const zenGotic = Zen_Kaku_Gothic_Antique({
+  subsets: ["cyrillic"],
+  weight: "400",
+});
